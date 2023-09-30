a7c7c97cb41d1a3b3dec6207032b61968b110f31
[Tatsumi0720] <escobandress@gmail.com>
2023-09-30 14:26:13 -0500

    relations finish

--- src/db.js
+++ src/db.js
@@ -53,8 +53,66 @@ const {
    Series,
    Ticket,
    User,
+   Rol,
+   Workpeople,
+   Requests,
 
 } = sequelize.models
+
+
+/////// ACA VIENEN LAS RELACIONES ///////
+
+//Usuario con la tabla seguidores
+User.hasMany(Follower, { as: 'customers', foreignKey: 'customersId' });
+User.hasMany(Follower, { as: 'organizers', foreignKey: 'organizersId' });
+
+//Rol con usuario
+Rol.hasMany(User);
+User.belongsTo(Rol);
+
+// Usuario con Review
+User.hasMany(Review);
+Review.belongsTo(User);
+
+// Tabla usuario con establecimiento
+User.hasMany(Establishment);
+Establishment.belongsTo(User);
+
+//User con Evento
+User.hasMany(Event);
+Event.belongsTo(User);
+
+//User con trabajadores
+User.hasMany(Workpeople, { as: 'workers', foreignKey: 'workersId' });
+User.hasMany(Workpeople, { as: 'organizer', foreignKey: 'organizerId' });
+
+//User con Ticket
+User.hasMany(Ticket);
+Ticket.belongsTo(User);
+
+//Establecimiento con Evento
+Establishment.hasMany(Event);
+Event.belongsTo(Establishment);
+
+//Evento y Trabjadores con la tabla Asigned
+Workpeople.hasMany(Assigned, { as: 'worker', foreignKey: 'workerId' });
+Event.hasMany(Assigned, { as: 'Event', foreignKey: 'EventId' });
+
+//Evento con las peticiones
+Requests.belongsTo(Event);
+Event.hasOne(Requests);
+
+//Evento con las series
+Event.hasMany(Series);
+Series.belongsTo(Event);
+
+//Series con Ticket
+Series.hasMany(Ticket);
+Ticket.belongsTo(Series);
+
+//Assigned con Tickets
+Assigned.hasMany(Ticket);
+Ticket.belongsTo(Assigned);
 
 
 /////// ACA VIENEN LAS RELACIONES ///////
