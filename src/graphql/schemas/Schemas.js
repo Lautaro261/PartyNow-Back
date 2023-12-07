e004e9e421fd0d64c345c9b261dc2ff6abc449e8
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-12-06 23:05:33 -0300

    format

--- src/graphql/schemas/Schemas.js
+++ src/graphql/schemas/Schemas.js
@@ -1,115 +1,113 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql `
-    #INPUT
-    input DatabaseLoad {
-        rols: Boolean!
-        typeofplaces: Boolean
-        clients: Boolean
-        organizers: Boolean
-        places: Boolean
-    }
+const typeDefs = gql`
+  #INPUT
+  input DatabaseLoad {
+    rols: Boolean!
+    typeofplaces: Boolean
+    clients: Boolean
+    organizers: Boolean
+    places: Boolean
+  }
 
-    input UserInput {
-        email: String!
-        password: String!
-        rol: ID!
-        username: String!
-        profilePhoto: String
-        coverPhoto: String
-        firstName: String
-        lastName: String
-        dni: String
-        phone: String
-        birthday: String
-    }
+  input UserInput {
+    email: String!
+    password: String!
+    rol: ID!
+    username: String!
+    profilePhoto: String
+    coverPhoto: String
+    firstName: String
+    lastName: String
+    dni: String
+    phone: String
+    birthday: String
+  }
 
-    input PlaceInput {
-        username:String!
-        typeOfPlace:ID!
-        nameOfPlace:String!
-        coverPhoto:String
-        profilePhoto: String
-        capacitance:Int
-        country:String!
-        province:String!
-        city: String!
-        addres:String!
-        numeration:String!
-        maps:String
-        longitude:String
-        latitude:String
-    }
+  input PlaceInput {
+    username: String!
+    typeOfPlace: ID!
+    nameOfPlace: String!
+    coverPhoto: String
+    profilePhoto: String
+    capacitance: Int
+    country: String!
+    province: String!
+    city: String!
+    addres: String!
+    numeration: String!
+    maps: String
+    longitude: String
+    latitude: String
+  }
 
-    input EventInput{
-        email: String!
-        place: ID
-        title: String!
-        description: String
-        date: String
-        time: String
-        capacitance: Int
-        serietitle: String!
-        price: String!
-        quantity: String!
+  input EventInput {
+    email: String!
+    place: ID
+    title: String!
+    description: String
+    date: String
+    time: String
+    capacitance: Int
+    serietitle: String!
+    price: String!
+    quantity: String!
+  }
 
-    }
+  #TYPES
+  type Message {
+    rols: String
+    typeofplaces: String
+    clients: String
+    organizers: String
+    places: String
+  }
 
-    #TYPES 
-    type Message{
-        rols: String
-        typeofplaces:String
-        clients:String
-        organizers:String
-        places: String
-    }
+  type Place {
+    localId: ID
+    localName: String
+    nextDate: String
+    nextTime: String
+    coverPhoto: String
+    profilePhoto: String
+    score: String
+    organizer: String
+    verified: Boolean
+    type: String
+  }
 
-    type Place{
-        localId: ID
-        localName: String
-        nextDate: String
-        nextTime: String
-        coverPhoto: String
-        profilePhoto: String
-        score: String
-        organizer: String
-        verified: Boolean
-        type: String
-    }
-
-    type PlaceCategory {
+  type PlaceCategory {
     category: String
     format: String
     places: [Place]
-}
+  }
 
-    # TYPE QUERY
-    type Query {
-        hello: String
+  # TYPE QUERY
+  type Query {
+    hello: String
 
-        #SIN AUTENTIFICACION
-        getAllplaces: [PlaceCategory]
+    #SIN AUTENTIFICACION
+    getAllplaces: [PlaceCategory]
 
+    #CON AUTENTIFICACION
+  }
 
-        #CON AUTENTIFICACION
-    }
+  #TYPE MUTATION
+  type Mutation {
+    #SOLO DEVS
+    databaseLoad(input: DatabaseLoad): Message
 
-    #TYPE MUTATION
-    type Mutation {
-        #SOLO DEVS
-        databaseLoad(input: DatabaseLoad): Message
+    #USUARIO
+    createUser(input: UserInput): String
 
-        #USUARIO
-        createUser(input: UserInput): String
+    #LUGARES
+    createPlace(input: PlaceInput): String
 
-        #LUGARES
-        createPlace(input: PlaceInput): String
+    #EVENTOS
+    createEvent(input: EventInput): String
 
-        #EVENTOS
-        createEvent(input: EventInput): String
-
-        #TICKETS
-    }
+    #TICKETS
+  }
 `;
 
 module.exports = typeDefs;
